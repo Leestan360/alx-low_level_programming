b86f87da0db0f486d56d8bdf4765ca04c3e1d849
Leestan360 <stanleymayore2@gmail.com>
2022-03-11 10:03:43 +0000

    Assigns a random number and prints it's last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 				n, a);
 	else if (a == 0)
 		printf("Last digit of %i is %i and is 0\n", n, a);
-	else
+	else if (a < 6 && a != 0)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n",
 				n, a);
 
