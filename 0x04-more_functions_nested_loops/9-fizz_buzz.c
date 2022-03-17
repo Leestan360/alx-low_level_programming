cc9ecd433b62efb08b19b1362510536972377b04
Leestan360 <stanleymayore2@gmail.com>
2022-03-17 11:34:45 +0000

    Prints numbers from 1 to 100 and 'Fizz ' where a multiple of 3 and 'Buzz ' where a multiple of 5 and 'FizzBuzz' where a multiple of both

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,7 +19,7 @@ int main(void)
 			printf("Fizz ");
 		else if (a % 5 == 0)
 			printf("Buzz ");
-		else if (a % 3 == 0 && a % 5 == 0)
+		else if (a % 15 == 0)
 			printf("FizzBuzz");
 		else
 			printf("%i ", a);
