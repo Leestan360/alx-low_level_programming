a99938a85577e1f54142e4c3807f0998fb5d5354
Leestan360 <stanleymayore2@gmail.com>
2022-03-17 11:48:26 +0000

    Prints numbers from 1 to 100 and 'Fizz ' where a multiple of 3 and 'Buzz ' where a multiple of 5 and 'FizzBuzz' where a multiple of both

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,8 @@
 
 /**
  * main - main block
- * prints "Fizz " for numbers divisible by 3,
- * prints "Buzz " for numbers divisible by 5,
+ * prints "Fizz" for numbers divisible by 3,
+ * prints "Buzz" for numbers divisible by 5,
  * prints "FizzBuzz" for numbers divisible by both 3 and 5
  * for numbers from 1 to 100
  * Return: 0
@@ -13,17 +13,17 @@ int main(void)
 {
 	int a;
 
-	for (a = 1; a <= 99; a++)
+	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0)
-			printf("Fizz ");
-		else if (a % 5 == 0)
-			printf("Buzz ");
-		else if (a % 15 == 0)
+		if (a % 15 == 0)
 			printf("FizzBuzz ");
+		else if ((a % 3) == 0)
+			printf("Fizz ");
+		else if ((a % 5) == 0)
+			printf("Buzz ");
 		else
-			printf("%i ", a);
+			print("%d ", a);
 	}
-	printf("Buzz\n");
+	printf("\n");
 	return (0);
 }
