0f8042b1f25627117c9d26b91c42c910f8e13435
Leestan360 <stanleymayore2@gmail.com>
2022-04-07 15:49:18 +0000

    Multiplies two positive numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,76 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _isdigit - checks for a non-digit in a string
+ * @s: string to evaluate
+ * Return: 0 if non-digit is found
+ * of 1 ontherwise
+ */
+
+int _isdigit(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		if (s[a] < '0' || s[a] > '9')
+			return (0);
+		a++;
+	}
+	return (1);
+}
+
+/**
+ * errors - returns the errors for main
+ */
+
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * strlen - returns the length of the string
+ * @s: string to evaluate
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	return (1);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: the arguments
+ * Return: 0 on Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	int mul = 1;
+
+	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
+		errors();
+	else
+	{
+		for (a = 1; a < argc; a++)
+		{
+			mul *= atoi(argv[a]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}
+
