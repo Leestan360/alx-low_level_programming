27858a3f9be052e8e47d36e0f5e0e4ed290937ff
Leestan360 <stanleymayore2@gmail.com>
2022-04-07 17:08:47 +0000

    Multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -58,44 +58,18 @@ void errors(void)
 
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, a, carry, digit1, digit2, *result, b = 0;
+	int a;
+	int mul = 1;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !_isdigit(s1) || !_isdigit(s2))
+	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
 		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
-		return (1);
-	for (a = 0; a <= len1 + len2; a++)
-		result[a] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	else
 	{
-		digit1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (a = 1; a < argc; a++)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
+			mul *= atoi(argv[a]);
 		}
-		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+		printf("%d\n", mul);
 	}
-	for (a = 0; a < len - 1; a++)
-	{
-		if (result[a])
-			b = 1;
-		if (b)
-			_putchar(result[a] + '0');
-	}
-	if (!b)
-		_putchar('0');
-	_putchar('\n');
-	free(result);
 	return (0);
 }
