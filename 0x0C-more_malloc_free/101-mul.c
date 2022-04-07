3e7fefe91b0f818ac26116863b75780ff7781ea6
Leestan360 <stanleymayore2@gmail.com>
2022-04-07 16:39:26 +0000

    Multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -23,17 +23,7 @@ int _isdigit(char *s)
 }
 
 /**
- * errors - returns the errors for main
- */
-
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
- * strlen - returns the length of the string
+ * _strlen - returns the length of the string
  * @s: string to evaluate
  * Return: length of the string
  */
@@ -50,6 +40,16 @@ int _strlen(char *s)
 }
 
 /**
+ * errors - handles errors in main
+ */
+
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: the arguments
@@ -58,19 +58,44 @@ int _strlen(char *s)
 
 int main(int argc, char *argv[])
 {
-	int a;
-	int mul = 1;
+	char *s1, *s2;
+	int len1, len2, len, a, carry, digit1, digit2, *result, b = 0;
 
-	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !_isdigit(s1) || !_isdigit(s2))
 		errors();
-	else
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (a = 0; a <= len1 + len2; a++)
+		result[a] = 0;
+	for (len1 = _strlen(s1) - 1; len1 >= 0; len1--)
 	{
-		for (a = 1; a < argc; a++)
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			mul *= atoi(argv[a]);
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		printf("%d\n", mul);
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
+	for (a = 0; a < len - 1; a++)
+	{
+		if (result[a])
+			b = 1;
+		if (b)
+			_putchar(result[a] + '0');
+	}
+	if (!b)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
-
